587f5a6d124ab98847bb2a164ba3c9d2994c24f4
daniel caamal <danielcaamal97@gmail.com>
2024-08-03 00:46:31 -0600

    FTR: Health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { OrdersModule } from './orders/orders.module';
 import { ProductsModule } from './products/products.module';
 import { TransportsModule } from './transports/transports.module';
 import { AuthModule } from './auth/auth.module';
+import { HealthCheckController } from './health-check/health-check.controller';
 
 @Module({
   imports: [ProductsModule, OrdersModule, TransportsModule, AuthModule],
-  controllers: [],
+  controllers: [HealthCheckController],
   providers: [],
 })
 export class AppModule {}
